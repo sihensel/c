175b30b47a0c7dbecfc96d5e3635246186b1c3ba
Simon Hensel <simon.hensel@protonmail.com>
2022-02-10 21:38:35 +0100

    Implement task completion
    * Fix the error regarding the modification of env variables

--- todo.c
+++ todo.c
@@ -6,35 +6,39 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define STR_SIZE 100
+#define STR_MAX 100
+#define PATH_MAX 256
 
-void add_task(char *filename, char *name);
-void show_tasks(char *filename);
-void complete_task(char *filename);
+void add_task(char *, char *);
+int show_tasks(char *);
+void complete_task(char *);
 
 int main(int argc, char *argv[])
 {
 
     // FIXME make this configurable
-    char *homedir = getenv("HOME");
-    char filename[STR_SIZE] = "/.todo";
+    char filename[PATH_MAX];
 
-    // concatenate the filename and the homedir into filename
-    strcpy(filename, strcat(homedir, filename));
+    // concatenate $HOME and "/.todo" into filename
+    strcat(strcpy(filename, getenv("HOME")), "/.todo");
 
     if (argc == 1) {
         show_tasks(filename);
-    } else {
+    }
+    else {
         if (strcmp(argv[1], "add") == 0) {
-            char name[STR_SIZE];
-            printf("Enter the task name\n");
-            fgets(name, STR_SIZE, stdin);
+            char name[STR_MAX];
+            printf("Enter the task name:\n");
+            fgets(name, STR_MAX, stdin);
             add_task(filename, name);
-        } else if (strcmp(argv[1], "done") == 0) {
-            complete_task(filename);
-        } else if (strcmp(argv[1], "list") == 0) {
+        }
+        else if (strcmp(argv[1], "list") == 0) {
             show_tasks(filename);
-        } else {
+        }
+        else if (strcmp(argv[1], "done") == 0) {
+            complete_task(filename);
+        }
+        else {
             printf("Unknown argument '%s'.\n", argv[1]);
         }
     }
@@ -48,7 +52,7 @@ void add_task(char *filename, char *task_name)
 
     // maybe remove this struct later
     struct {
-        char name[STR_SIZE];
+        char name[STR_MAX];
         unsigned int due;   // due day, Unix timestring? is int big enough?
     } task;
 
@@ -57,6 +61,7 @@ void add_task(char *filename, char *task_name)
 
     len = strlen(task.name);
 
+    // null-terminate newline strings, as produced by fgets()
     if (task.name[len - 1] == '\n')
         task.name[len - 1] = '\0';
 
@@ -71,7 +76,7 @@ void add_task(char *filename, char *task_name)
     return;
 }
 
-void show_tasks(char *filename)
+int show_tasks(char *filename)
 {
     FILE *fp;
     char *line = NULL;
@@ -83,41 +88,98 @@ void show_tasks(char *filename)
     fp = fopen(filename, "r");
     if (fp == NULL) {
         printf("Could not open %s\n", filename);
-        return;
+        return 0;
     }
 
+    // read the file the first time to determine the longest task name
+    // FIXME is there a better solution to this?
     while (getline(&line, &len, fp) != -1) {
         token = strtok(line, DELI);
         if (strlen(token) > max_tok_len)
             max_tok_len = strlen(token);
     }
-
+    // output headers
     printf("INDEX\t%-*s\tDUE\n", max_tok_len, "TASK");
 
+    // read the file a second time to render it to the user
     fp = fopen(filename, "r");
     int counter = 0;
     while (getline(&line, &len, fp) != -1) {
         token = strtok(line, DELI);
-        printf("%d\t", counter);
+        printf("%d\t", counter + 1);
 
         // parse all tokens in a line, separated by DELI
         while (token != NULL) {
-            if (strchr(token, 10)) {
+            if (strchr(token, '\n')) {
                 printf("%s", token);
-            } else {
+            }
+            else {
                 printf("%-*s\t", max_tok_len, token);
             }
             token = strtok(NULL, DELI);
         }
         counter ++;
     }
-
     fclose(fp);
     free(line);
 
-    return;
+    return counter;
 }
 
-void complete_task(char *filename) {
+void complete_task(char *filename)
+{
+    unsigned int index = 0;
+    unsigned int no_of_tasks = 0;
+
+    no_of_tasks = show_tasks(filename);
+
+    if (no_of_tasks == 0) {
+        return;
+    }
+
+    printf("\nSpecify the index of the task to complete:\n");
+    scanf("%d", &index);
+
+    if (index < 1 || index > no_of_tasks) {
+        printf("Invalid input (please specify a valid index).\n");
+        return;
+    }
+    else {
+        // FIXME writing to a tempfile for now
+        // can be replaced by loading the whole file into the buffer
+        char tmpfile[PATH_MAX];
+        FILE *fp = NULL;
+        FILE *fp_tmp = NULL;
+        char str[STR_MAX];
+        int counter= 0;
+
+        strcat(strcpy(tmpfile, getenv("HOME")), "/.todo_TMP");
+
+        fp = fopen(filename, "r");
+        fp_tmp = fopen(tmpfile, "w");
+        if (fp == NULL || fp_tmp == NULL) {
+            printf("Could not open file %s or %s\n", filename, tmpfile);
+            return;
+        }
+
+        // read the original file
+        while (!feof(fp)) {
+            strcpy(str, "\0");
+            fgets(str, STR_MAX, fp);
+
+            if (!feof(fp)) {
+                counter ++;
+
+                // skip the line that should be removed
+                if (counter != index)
+                    fprintf(fp_tmp, "%s", str);
+            }
+        }
+        fclose(fp);
+        fclose(fp_tmp);
+
+        remove(filename);
+        rename(tmpfile, filename);
+    }
     return;
 }
